8c831310cf562d1d8847ea8e7a246c97b2849bb0
guilhermo <guilhermo.readonly@gmail.com>
2021-06-22 13:15:47 +0200

    modify shell.nix...

--- shell.nix
+++ shell.nix
@@ -3,14 +3,6 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "rust-env";
   nativeBuildInputs = [
-    rustup
-
-    pkgs.alsaLib
-    pkgs.openssl
-    pkgs.libudev
-    pkgs.pkg-config
-    systemd.dev
-
     # Example Build-time Additional Dependencies
     # pkg-config
     # systemd.dev
@@ -19,6 +11,21 @@ stdenv.mkDerivation {
   buildInputs = [
     # Example Run-time Additional Dependencies
     # openssl
+    
+    rustc
+    cargo
+    alsaLib
+    openssl
+    libudev
+    pkg-config
+    systemd.dev
+    lldb
+    gcc
+    glibc
+    clang
+    pkgconfig
+    capnproto
+    gnumake
   ];
 
   # Set Environment Variables
